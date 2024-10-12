bb756dab85e3b065cc11bcf40997639310f57629
sojfowpw <alexandra.plisova@gmail.com>
2024-10-12 00:57:47 -0400

    update parsing

--- parsing_json.hpp
+++ parsing_json.hpp
@@ -6,7 +6,35 @@
 
 using namespace std;
 using json = nlohmann::json;
-namespace fs = std::filesystem;
+namespace fs = filesystem;
+
+int tableSize; // размер колонок
+
+void removeDirectory(const fs::path& directoryPath) { // удаление директории
+    if (fs::exists(directoryPath)) { // проверка, существует ли она вообще
+        fs::remove_all(directoryPath); // удаление
+    }
+}
+
+void createDirectoriesAndFiles(const fs::path& schemePath, const json& structure) { // создание полной директории и файлов
+    for (const auto& table : structure.items()) { // цикл по всем элементам в структуре json
+        fs::path tablePath = schemePath / table.key(); // key - имя таблицы
+        if (!fs::create_directory(tablePath)) {
+            cerr << "Не удалось создать директорию: " << tablePath << endl;
+            return;
+        }
+        cout << "Создана директория: " << tablePath << endl;
+
+        fs::path csvFilePath = tablePath / (table.key() + ".csv"); // создаём csv файлы с названием таблиц
+        ofstream csvFile(csvFilePath);
+        if (!csvFile.is_open()) {
+            cerr << "Не удалось создать файл: " << csvFilePath << endl;
+            return;
+        }
+        csvFile.close();
+        cout << "Создан файл: " << csvFilePath << endl;
+    }
+}
 
 void parsing() {
     string filename = "schema.json"; // название файла
@@ -29,10 +57,15 @@ void parsing() {
 
     string schemeName = jparsed["name"]; // извлекаем имя схемы
     fs::path schemePath = fs::current_path() / schemeName; // формируем путь к директории
+    removeDirectory(schemePath); // удаляем, чтобы заново создать директорию
     if (!fs::create_directory(schemePath)) { // проверка
         cerr << "Не удалось создать директорию: " << schemePath << endl;
         return;
     }
     cout << "Создана директория: " << schemePath << endl;
-    return;
+    if (jparsed.contains("structure")) { // наполнение директории
+        createDirectoriesAndFiles(schemePath, jparsed["structure"]);
+    }
+    tableSize = jparsed["tuples_limit"]; // вытаскиваем ограничения по строкам
+    cout << endl << tableSize;
 }
