df417660e162a26396beb8d01b64b20b034dfa35
sojfowpw <alexandra.plisova@gmail.com>
2024-10-14 13:12:46 +0700

    update

--- main.cpp
+++ main.cpp
@@ -1,11 +1,51 @@
-#include <iostream>
-#include <fstream>
 #include "parsing_json.hpp"
+#include "structure.hpp"
 
-using namespace std;
-using json = nlohmann::json;
+enum class Commands { // существующие команды
+    EXIT,
+    INSERT,
+    DELETE,
+    SELECT,
+    UNKNOWN
+};
+
+Commands stringToCommand(const string& str) { // определение команд
+    if (str == "EXIT") {
+        return Commands::EXIT;
+    }
+    else if (str == "INSERT") {
+        return Commands::INSERT;
+    }
+    else if (str == "DELETE") {
+        return Commands::DELETE;
+    }
+    else if (str == "SELECT") {
+        return Commands::SELECT;
+    }
+    else {
+        cerr << "Неизвестная команда.\n";
+        return Commands::UNKNOWN;
+    }
+}
 
 int main() {
     parsing();
+    cout << "\n\n";
+    string command; // строка для команды
+    while (true) {
+        cout << "Введите команду: ";
+        getline(cin, command);
+        if (command == "") { // если пустая строка
+            continue;
+        }
+        Commands cmd = stringToCommand(command); // обработка введённой команды
+        switch (cmd) {
+            case Commands::EXIT: // выход
+                return 0;
+            case Commands::INSERT: // вставка
+                insert(command);
+                break;
+        }
+    }
     return 0;
 }
--- parsing_json.hpp
+++ parsing_json.hpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <filesystem> // работа с файловой системой
 #include "json.hpp"
+#include "structure.hpp"
 
 using namespace std;
 using json = nlohmann::json;
@@ -16,27 +17,79 @@ void removeDirectory(const fs::path& directoryPath) { // удаление дир
     }
 }
 
-void createDirectoriesAndFiles(const fs::path& schemePath, const json& structure) { // создание полной директории и файлов
+tNode* createDirectoriesAndFiles(const fs::path& schemePath, const json& structure) { // создание полной директории и файлов
+    tNode* tableHead = nullptr; // указатель на первый узел списка таблиц
+    tNode* tableTail = nullptr; // указатель на последний узел списка таблиц
+
     for (const auto& table : structure.items()) { // цикл по всем элементам в структуре json
         fs::path tablePath = schemePath / table.key(); // key - имя таблицы
         if (!fs::create_directory(tablePath)) {
             cerr << "Не удалось создать директорию: " << tablePath << endl;
-            return;
         }
         cout << "Создана директория: " << tablePath << endl;
 
-        fs::path csvFilePath = tablePath / (table.key() + ".csv"); // создаём csv файлы с названием таблиц
+        tNode* newTable = new tNode{table.key(), nullptr, nullptr}; // создаём таблицу
+        fs::current_path(tablePath); // переходим в папку таблицы
+        string lock = table.key() + "_lock.txt"; // создаём файл блокировки
+        ofstream file(lock);
+        if (!file.is_open()) {
+            cerr << "Не удалось открыть файл.\n";
+        }
+        file << "unlocked"; // по умолчанию разблокировано
+        file.close();
+        if (tableHead == nullptr) { // добавляем таблицу в список
+            tableHead = newTable; // если список пустой, таблица будет и первой, и последней одновременно
+            tableTail = newTable;
+        }
+        else {
+            tableTail->next = newTable; // иначе добавляем новую таблицу в конец списка
+            tableTail = newTable; 
+        }
+
+        string keyColumn = table.key() + "_pk"; // название специальной колонки
+        Node* column_pk = new Node{keyColumn, nullptr}; // создаём список, где специальная колонка - первая
+        newTable->column = column_pk; // присоединяем список колонок к таблице
+
+        fs::path csvFilePath = tablePath / "1.csv"; // создаём csv файл
         ofstream csvFile(csvFilePath);
         if (!csvFile.is_open()) {
             cerr << "Не удалось создать файл: " << csvFilePath << endl;
-            return;
         }
+        csvFile << keyColumn << ",";
+        const auto& columns = table.value(); // запись колонок в файл, объект columns = названия
+        for (size_t i = 0; i < columns.size(); ++i) { 
+            csvFile << columns[i].get<string>(); // записываем названия без кавычек
+            Node* newColumn = new Node{columns[i], nullptr}; // создаём новую колонку
+            if (newTable->column == nullptr) { // если в таблице ещё нет колонок
+                newTable->column = newColumn;
+            }
+            else {
+                Node* lastColumn = newTable->column;
+                while (lastColumn->next != nullptr) { // ищем последнюю колонку
+                    lastColumn = lastColumn->next;
+                }
+                lastColumn->next = newColumn; // добавляем новую колонку в конец
+            }
+            if (i < columns.size() - 1) { // для последнего значения не нужна запятая
+                csvFile << ",";
+            }
+        }
+        csvFile << endl;
         csvFile.close();
         cout << "Создан файл: " << csvFilePath << endl;
+
+        string pk = keyColumn + "_sequence.txt"; // создаём файл для хранения уникального первичного ключа
+        ofstream filePk(pk);
+        if (!filePk.is_open()) {
+            cerr << "Не удалось открыть файл.\n";
+        }
+        filePk << "0";
+        filePk.close();
     }
+    return tableHead;
 }
 
-void parsing() {
+void parsing() { // парсинг схемы
     string filename = "schema.json"; // название файла
     ifstream file(filename); // открываем
     if (!file.is_open()) {
@@ -52,8 +105,6 @@ void parsing() {
 
     json jparsed; // хранение результата парсинга
     jparsed = json::parse(json_content); // парсинг в объект jparsed
-    cout << "Парсинг json:" << endl;
-    cout << jparsed.dump(4) << endl; // вывод с форматированием
 
     string schemeName = jparsed["name"]; // извлекаем имя схемы
     fs::path schemePath = fs::current_path() / schemeName; // формируем путь к директории
@@ -67,5 +118,4 @@ void parsing() {
         createDirectoriesAndFiles(schemePath, jparsed["structure"]);
     }
     tableSize = jparsed["tuples_limit"]; // вытаскиваем ограничения по строкам
-    cout << endl << tableSize;
 }
