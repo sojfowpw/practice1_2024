967512b4bb09a85ef9eac661cf20550b9d262693
sojfowpw <alexandra.plisova@gmail.com>
2024-10-25 01:11:24 +0700

    update

--- select.h
+++ select.h
@@ -6,4 +6,9 @@
 using namespace std;
 
 void splitDot(const string& word, string& table, string& column, tableJson& tjs);
+string ignoreQuotes(const string& word);
+bool findDot(const string& word);
+int countCsv(tableJson& tjs, const string& table);
+void crossJoin(tableJson& tjs, const string& table1, const string& table2, const string& column1, const string& column2);
+bool checkCond(tableJson& tjs, const string& table, const string& column, const string& tcond, const string& ccond, const string& s);
 void select(const string& command, tableJson& tjs);
